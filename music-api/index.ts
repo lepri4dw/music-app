87a77c8ec180c28d74007b328eb1d0a60ae1b255
Aleksey Ivanov <lepri4dw@gmail.com>
2023-02-16 15:50:25 +0600

    Added users router

--- music-api/index.ts
+++ music-api/index.ts
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import artistsRouter from "./routers/artists";
 import albumsRouter from "./routers/albums";
 import tracksRouter from "./routers/tracks";
+import usersRouter from "./routers/users";
 
 const app = express();
 const port = 8000;
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use('/artists', artistsRouter);
 app.use('/albums', albumsRouter);
 app.use('/tracks', tracksRouter);
+app.use('/users', usersRouter);
 
 
 const run = async () => {
---
+++ music-api/routers/users.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import {Error} from "mongoose";
+import User from "../models/User";
+
+const usersRouter = express.Router();
+
+usersRouter.post('/', async (req, res, next) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    user.generateToken();
+    await user.save();
+    return res.send(user);
+  } catch (error) {
+    if (error instanceof Error.ValidationError) {
+      return res.sendStatus(400).send(error);
+    }
+
+    return next(error);
+  }
+});
+
+usersRouter.post('/sessions', async (req, res) => {
+  const user = await User.findOne({username: req.body.username});
+
+  if (!user) {
+    return res.status(400).send({error: 'Username not found'});
+  }
+
+  const isMatch = await user.checkPassword(req.body.password);
+
+  if (!isMatch) {
+    return res.status(400).send({error: 'Password is wrong'});
+  }
+
+  user.generateToken();
+  await user.save();
+
+  return res.send({message: 'Username and password correct!', user});
+});
+
+export default usersRouter;
